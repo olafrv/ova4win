9ffbd685f273e31e4ec0cf01d04ae2ec0146803b
Olaf Reitmaier <olafrv@gmail.com>
2024-04-07 22:52:51 +0200

    Generic comment

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # sudo apt install smbclient
-smbclient '\\ds220.lan\public' -U '%'-N -c 'prompt OFF;recurse ON; mput dist'
+# smbclient '\\mySambaServer\public' -U '%'-N -c 'prompt OFF;recurse ON; mput dist'
